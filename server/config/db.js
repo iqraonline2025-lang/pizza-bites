1cd6eac164e2fd87bb08af6b0cef56893a84a9ca
RESTADAN <adanrehman681@gmail.com>
2026-04-30 20:19:31 +0100

    improve db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      console.error("MONGO_URI is not defined");
+      process.exit(1);
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB connected");
   } catch (err) {
-    console.log(err);
+    console.error("MongoDB connection failed:", err.message);
     process.exit(1);
   }
 };
